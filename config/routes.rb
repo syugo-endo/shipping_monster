1e11a9a28a6fa6b0855ad0a186b6d1417e2c0a09
syugo-endo <1999endou@gmail.com>
2020-08-24 11:09:35 +0900

    Revert "deviseに関するビュー配置"
    This reverts commit e2a7132259f4b908f7b4f73ba0f792064e7146f8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/homepage'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#homepage'
 end
