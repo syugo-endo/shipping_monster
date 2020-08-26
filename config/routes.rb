40d17d3228f78a25831cf59d4eb1a75a0ccc5cc7
syugo-endo <1999endou@gmail.com>
2020-08-26 17:31:21 +0900

    googlemap 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'maps/index'
   devise_for :users
   get 'welcome/homepage'
   root 'welcome#homepage'
   resources :contacts, only: [:new, :create]
+  resources :maps, only: [:index]
 end
