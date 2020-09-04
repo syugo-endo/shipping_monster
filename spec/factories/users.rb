7e8141bf2165db9e76f2532a64e5d0223e88166a
syugo-endo <1999endou@gmail.com>
2020-09-04 17:35:31 +0900

    userとcontactのテストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :user do
-    nickname {}
-    email {}
-    password {}
-    password_confirmation {password}
+    nickname              { 'kakaka' }
+    email                 { 'testman@co.jp' }
+    password              { 'kakaka3' }
+    password_confirmation { password }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    birthday              { '1999-10-10' }
   end
 end
